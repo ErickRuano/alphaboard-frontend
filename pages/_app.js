c8327195d7f10b52c2bcaee0593782c5a855ad5d
jaqabalix@gmail.com <jaqabalix@gmail.com>
2022-02-25 03:03:32 -0600

    Main component handles private pages

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,26 @@
 import { ThemeProvider } from 'next-themes'
+import Session from './_session';
+import { useRouter } from 'next/router';
 
 import '../styles/globals.css'
 
+const publicPages = ['/'];
+
+
 function MyApp({ Component, pageProps }) {
+  const { pathname } = useRouter();
+  const isPublicPage = publicPages.includes(pathname);
   return (
     <ThemeProvider defaultTheme="fantasy">
-      <Component {...pageProps} />
+      {isPublicPage ? (
+          <Component {...pageProps} />
+        ) : (
+          <>
+              <Session>
+                  <Component {...pageProps} />
+              </Session>
+          </>
+        )}
     </ThemeProvider>
   )
 }
